f638aace859c2aca0ee749dcf585237caebda58e
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-10-07 21:42:31 +0500

    add Controller

---
+++ UserApiWithDb/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace UserApiWithDb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : RESTFulController
+    {
+        
+    }
+}
